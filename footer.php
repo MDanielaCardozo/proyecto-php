ec727f5a04b2ad4a84924ec84634e04ac735e8d5
Daniela Cardozo <mdanielacardozo@gmail.com>
2024-08-15 14:05:50 -0300

    homepage, header y footer

---
+++ footer.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <div class=" container position-absolute bottom-0 start-50 translate-middle-x">
+        <p class="text-color fw-bold">Moca Bakery fue fundada en 1991. Produce y elabora todos sus productos con materia de primera calidad. Son recién horneados y deliciosos.</p>
+        <hr class="text-white">
+        <div class="d-flex flex-column flex-lg-row justify-content-between align-items-center">
+            <div class="ms-lg-0 ms-auto me-auto text-center">
+                <p class="text-white">&copy;2024 Moca Bakery</p>
+            </div>
+            <a class="navbar-link" href=".">Politica de Privacidad</a>
+            <a class="navbar-link" href=".">Terminos y condiciones</a>
+            <a class="navbar-link" href=".">Contacto</a>
+            <div>
+
+            </div>
+        </div>
+    </div>
+</div>
--- header.php
+++ header.php
@@ -1,3 +1,41 @@
-<header>
-    
-</header>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Panaderia Moca">
+    <meta name="keywords" content="Panaderia, Pan, Pancake, Medialunas">
+    <meta name="author" content="Daniela Cardozo">
+    <title>Panaderia Moca</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg justify-content-around m-4">
+            <div class="container">
+                <a class="navbar-logo text-bold fs-3 fw-bold text-center p-3" href="index.php">Moca Bakery</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link text-light active" aria-current="page" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="productos.php">Productos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="contacto.php">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
